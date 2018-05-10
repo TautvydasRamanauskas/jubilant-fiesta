7180565f4ea1ea33161913f77c75bc261480ddbf
TautvydasRa <ta.ramanauskas@ktu.edu>
2018-05-10 14:43:43 +0300

    [#25] Add Authorization to header

--- src/services/call-service.js
+++ src/services/call-service.js
@@ -1,16 +1,11 @@
 import {API_URL} from '../config';
 
 const call = (endpoint, method, data) => {
-    // if (method !== 'GET') {
-    //     data = {
-    //         ...data,
-    //         key: '8NpIU1YWqm-z1tvRLEeJt-jRoYbl1qf5',
-    //     };
-    // }
     return fetch(API_URL + endpoint, {
         body: JSON.stringify(data),
         headers: {
-            'content-type': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic YzYxM2FmZGItODhiMi00OWI5LThlNWEtY2UyMjRmMDcyMmNjOjllZTEyNDhmLWNlYWMtNGUzYS1hODg2LWE4YWYzNDA4NTMxNg==',
         },
         method,
     })
